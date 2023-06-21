61d0347baba9a64d2daf58e2c7cf1f823d465aae
Tooth Toothon <aheffer1@binghamton.edu>
2023-06-20 23:54:28 -0400

    fixed fonts and nav bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,57 +1,71 @@
 import React from 'react'
 import "./Header.css";
+import classNames from 'classnames';
+import useSticky from './useSticky';
 
 const Header = () => {
+    const { sticky, stickyRef } = useSticky();
     return (
-      <header>
-          <nav>
-            <ul>
-              <li><a href="/">Medications</a></li>
-              <li><a href="/">Diabetics</a></li>
-              <li><a href="/">Women's Health</a></li>
-              <li><a href="/">Men's Health</a></li>
-              <li><a href="/">Cholesterol</a></li>
-              <li><a href="/">Cold Sores</a></li>
-              <li><a href="/">Birth Control</a></li>
-              <li><a href="/">Hair Loss</a></li>
-              <li><a href="/">Erectile Dysfunction</a></li>
-              <li><a href="/">Pets Care</a></li>
-              <li><a href="/">Formulary</a></li>
-            </ul>
-          </nav>
-        <div className="actualheader">
-          <div className="logo"></div>
-          <div className='search'>
-            <div className='input'>
-              <div className='location'>
-                <div className='location-text'>
-                  Deliver to 38006
+      <div>
+        <header>
+          <div className="actualheader">
+            <div className="logo">
+              <a href="/">
+              </a>
+            </div>
+            <div className='search'>
+              <div className='input'>
+                <div className='location'>
+                  <div className='location-text'>
+                    Deliver to 38006
+                  </div>
+                </div>
+                <div className='line1'></div>
+                <div className='searchtext'>
+                  Search for medications like lipitor
                 </div>
               </div>
-              <div className='line1'></div>
-              <div className='searchtext'>
-                Search for medications like lipitor
-              </div>
             </div>
-          </div>
-          <div className='right'>
-            <div className='buttons'>
-              <div className='icon-buttons'>
-                <div className='basket-shopping'>
+            <div className='right'>
+              <div className='buttons'>
+                <div className='icon-buttons'>
+                  <div className='basket-shopping'>
 
+                  </div>
                 </div>
+                <button class="whitebutton">
+                  Sign In
+                </button>
+                <button class="bluebutton">
+                  Get started
+                </button>
               </div>
-              <button class="whitebutton">
-                Sign In
-              </button>
-              <button class="bluebutton">
-                Get started
-              </button>
             </div>
+            <div className="ellipse1"></div>
           </div>
-          <div className="ellipse1"></div>
-        </div>
-      </header>
+        </header>
+                  <nav ref={stickyRef} className={classNames('nav', { sticky })}>
+                    <ul>
+                      <li><a href="/">Medications</a></li>
+                      <li><a href="/">Diabetics</a></li>
+                      <li><a href="/">Women's Health</a></li>
+                      <li><a href="/">Men's Health</a></li>
+                      <li><a href="/">Cholesterol</a></li>
+                      <li><a href="/">Cold Sores</a></li>
+                      <li><a href="/">Birth Control</a></li>
+                      <li><a href="/">Hair Loss</a></li>
+                      <li><a href="/">Erectile Dysfunction</a></li>
+                      <li><a href="/">Pets Care</a></li>
+                      <li><a href="/">Formulary</a></li>
+                    </ul>
+                </nav>
+                <div
+              style={{
+                height: sticky ? `${stickyRef.current?.clientHeight}px` : '0px',
+              }}
+            />
+
+      </div>
     )
 };
 
